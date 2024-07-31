0fd15b82a9e8d0581f3293b97538727e25227fa4
agungputra <116995331+agungputraa@users.noreply.github.com>
2024-07-31 09:36:19 +0800

    Update KucingGarong.js

--- KucingGarong.js
+++ KucingGarong.js
@@ -408,35 +408,7 @@ window.filterVideos = function() {
  
 });
 
-var myFP = fluidPlayer(
-        'video-id',	{
-	"layoutControls": {
-		"controlBar": {
-			"autoHideTimeout": 3,
-			"animated": true,
-			"autoHide": true
-		},
-		"htmlOnPauseBlock": {
-			"html": null,
-			"height": null,
-			"width": null
-		},
-		"autoPlay": false,
-		"mute": true,
-		"allowTheatre": true,
-		"playPauseAnimation": true,
-		"playbackRateEnabled": true,
-		"allowDownload": false,
-		"playButtonShowing": true,
-		"fillToContainer": false,
-		"posterImage": ""
-	},
-	"vastOptions": {
-		"adList": [],
-		"adCTAText": false,
-		"adCTATextPosition": ""
-	}
-});
+
 
 
  
